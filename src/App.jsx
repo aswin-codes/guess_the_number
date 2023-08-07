efec016028dd740c0f9879b26655e56eee7f2b53
aswin-codes <codicon.co@gmail.com>
2023-08-07 10:38:14 +0530

    Fixed footer

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,9 @@ function App() {
       <main className="flex-1 flex justify-center items-center font-nunito p-10">
         <Card/>
       </main>
+      <footer className="flex p-4 text-gray-100 text-xs justify-center" >
+        <p>Designed and developed by</p> <a href="https://www.instagram.com/nscc_vitcc/" className="ml-2 font-bold underline"> NSCC</a>
+      </footer>
     </div>
   );
 }
